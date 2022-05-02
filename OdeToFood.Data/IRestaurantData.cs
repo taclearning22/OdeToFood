8ba49cf3262c22dd10cc2475a6d5fd6181784704
pagawde <pagawde@DESKTOP-GLI6O6O>
2022-05-02 19:05:45 +0530

    Add project files.

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+    public class InMemoryRestaurantData: IRestaurantData
+    {
+        public List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{Id = 1, Name = "Ciao", Location = "Houston", Cuisine = CuisineType.Italian },
+                new Restaurant{Id = 2, Name = "Hola", Location = "Washington", Cuisine = CuisineType.Mexican},
+                new Restaurant{Id = 3, Name = "Namaste", Location = "California", Cuisine = CuisineType.Indian}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
